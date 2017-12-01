ea46a9ffaf04335a57cd7c88b4820f35cccc2539
Parvez Rafi <rafiparvez@gmail.com>
2017-12-01 12:29:23 -0600

    initial commit

---
+++ DB_Scripts/get_readmission_details_2.sql
@@ -0,0 +1,16 @@
+set search_path to mimiciii;
+
+drop materialized view if exists admission_details;
+create materialized view admission_details as
+  select ad.subject_id, ad.hadm_id, ad.admittime, ad.dischtime, ad.deathtime
+  , ie.icustay_id, intime, ie.outtime, ie.last_wardid, ie.first_careunit, ie.last_careunit
+  , extract(epoch from (ad.admittime - p.dob))/60.0/60.0/24.0/365.242 as age
+  , p.gender as gender
+  , ad.marital_status as marital_status
+  , ad.insurance as insurance
+  from admissions ad
+    inner join icustays ie
+	on ad.hadm_id = ie.hadm_id
+    inner join patients p
+    on ad.subject_id = p.subject_id
+  order by ad.subject_id,ad.hadm_id;
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ICUReadmissionPrediction
+# Interpretable Deep Learning for Predicting 30-Day Hospital Readmission  
+
+The repository contains work related to predicting 30-day all-cause hospital readmissions.
